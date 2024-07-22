fef66cc57bae0b1c86f1e9fe59d7fffc6b474425
hassanelq <hassanelqadi2@gmail.com>
2024-07-22 21:04:28 +0100

    chore: Refactor get_estimated_price function to include additional features

--- server/util.py
+++ server/util.py
@@ -10,27 +10,30 @@ __data_columns = None
 __model = None
 
 def get_estimated_price(location, type, status, property_state, area, rooms, bedrooms, bathrooms, jardin, piscine, cuisine_equiped):
+    if __data_columns is None:
+        raise ValueError("Data columns are not loaded. Please check if load_saved_artifacts() has been called.")
+
     try:
         print("Received in get_estimated_price:")
         print(f"location={location}, type={type}, status={status}, property_state={property_state}, area={area}, rooms={rooms}, bedrooms={bedrooms}, bathrooms={bathrooms}, jardin={jardin}, piscine={piscine}, cuisine_equiped={cuisine_equiped}")
         
         loc_index = __data_columns.index(location.lower())
-    except:
+    except ValueError:
         loc_index = -1
     
     try:
         type_index = __data_columns.index(type.lower())
-    except:
+    except ValueError:
         type_index = -1
     
     try:
         status_index = __data_columns.index(status.lower())
-    except:
+    except ValueError:
         status_index = -1
     
     try:
         property_state_index = __data_columns.index(property_state.lower())
-    except:
+    except ValueError:
         property_state_index = -1
 
     x = np.zeros(len(__data_columns))
@@ -64,17 +67,27 @@ def load_saved_artifacts():
     global __status
     global __property_states
 
-    with open("./artifacts/columns.json", "r") as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[20:]
-        __types = __data_columns[7:10]
-        __status = __data_columns[10:13]
-        __property_states = __data_columns[13:20]
+    try:
+        with open("./artifacts/columns.json", "r") as f:
+            __data_columns = json.load(f)['data_columns']
+            __locations = __data_columns[20:]
+            __types = __data_columns[7:10]
+            __status = __data_columns[10:13]
+            __property_states = __data_columns[13:20]
+            print(f"Loaded data columns: {__data_columns}")
+    except Exception as e:
+        print(f"Error loading columns.json: {e}")
+        __data_columns = []
 
     global __model
     if __model is None:
-        with open('./artifacts/Agadir_home_prices_model.pickle', 'rb') as f:
-            __model = pickle.load(f)
+        try:
+            with open('./artifacts/Agadir_home_prices_model.pickle', 'rb') as f:
+                __model = pickle.load(f)
+            print("Model loaded successfully")
+        except Exception as e:
+            print(f"Error loading model: {e}")
+
     print("loading saved artifacts...done")
 
 def get_data_columns():
