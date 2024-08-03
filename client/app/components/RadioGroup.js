7371cb5eb75466e6772f80c860db081c57e7ecf1
hassanelq <hassanelqadi2@gmail.com>
2024-08-03 13:11:49 +0100

    refactor: Update RadioGroup styling to use gray background color instead of green

--- client/app/components/RadioGroup.js
+++ client/app/components/RadioGroup.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const RadioGroup = ({ name, options, selectedValue, onChange }) => {
   return (
-    <div className="relative flex items-center justify-center rounded-full bg-white text-black overflow-hidden border border-gray-300 w-full">
+    <div className="relative flex items-center justify-center rounded-full bg-white text-black overflow-hidden border border-gray-300 w-full h-[40px]">
       {options.map((option) => (
         <label
           key={option}
-          className={`flex-1 py-2 cursor-pointer flex justify-center items-center z-10 font-semibold text-sm rounded-full transition-all duration-200 ${
+          className={`flex-1 py-2 cursor-pointer flex justify-center items-center z-10 font-semibold text-sm rounded-full transition-all duration-200 h-full ${
             selectedValue === option
               ? "bg-gray-600 text-white"
               : "bg-gray-200 text-[#4b4b4b]"
